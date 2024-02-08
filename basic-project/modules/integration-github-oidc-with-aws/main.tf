01b1ab3fde9c0fe17fe8bc485cf236ba557f9e7c
text-adi <alosha123456789049@gmail.com>
2024-02-08 17:07:15 +0200

    Added first basic configuration

--- basic-project/modules/integration-github-oidc-with-aws/main.tf
+++ basic-project/modules/integration-github-oidc-with-aws/main.tf
@@ -1,15 +1,6 @@
-module "iam_github_oidc_provider" {
-  source  = "terraform-aws-modules/iam/aws//modules/iam-github-oidc-provider"
-  version = "5.34.0"
-  create = false
-
-}
-
 module "iam_iam-github-oidc-role" {
   source  = "terraform-aws-modules/iam/aws//modules/iam-github-oidc-role"
   version = "5.34.0"
-
-  provider_url = module.iam_github_oidc_provider.url
 
   name     = "github-actions-role-${var.name-project}-${var.environment}"
   subjects = var.github-repo
